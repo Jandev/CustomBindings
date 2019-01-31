abf62eef562a90c75e95339c68c6aeb8708758e6
Jan de Vries <jan@jan-v.nl>
2019-01-31 21:24:46 +0100

    added httpclient

---
+++ HttpBinding/HttpAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Description;
+
+namespace HttpBinding
+{
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    [Binding]
+    public class HttpAttribute : Attribute
+    {
+        [AutoResolve]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// If you have already instantiated an <seealso cref="System.Net.Http.HttpClient"/>, pass it to this property.
+        /// </summary>
+        public HttpClient HttpClient { get; set; }
+    }
+}
---
+++ HttpBinding/HttpBinding.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Description;
+using Microsoft.Azure.WebJobs.Host.Config;
+
+namespace HttpBinding
+{
+    [Extension(nameof(HttpBinding))]
+    public class HttpBinding : IExtensionConfigProvider
+    {
+        private static HttpClient httpClient;
+
+        public void Initialize(ExtensionConfigContext context)
+        {
+            var rule = context.AddBindingRule<HttpAttribute>();
+            rule.BindToInput<HttpModel>(BuildFromAttribute);
+        }
+
+        private async Task<HttpModel> BuildFromAttribute(
+            HttpAttribute attribute,
+            CancellationToken cancellationToken)
+        {
+            if (httpClient == null)
+            {
+                httpClient = attribute.HttpClient ?? new HttpClient();
+            }
+
+            var response = await httpClient.GetAsync(attribute.Url, cancellationToken);
+
+            return new HttpModel
+            {
+                Response = await response.Content.ReadAsStringAsync(),
+                ResponseCode = (int)response.StatusCode
+            };
+        }
+    }
+}
