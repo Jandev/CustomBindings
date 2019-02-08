eb1b565d6caf9696fb48d3d41dda8016af9e4cfc
Jan de Vries <jan@jan-v.nl>
2019-02-08 11:19:52 +0100

    Added httpbinding function

--- CustomBindingFunction/CorrectImplementation.cs
+++ CustomBindingFunction/CorrectImplementation.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using HttpBinding;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -11,7 +12,8 @@ namespace CustomBindingFunction
         private static readonly HttpClient HttpClient = new HttpClient();
 
         [FunctionName("CorrectImplementation")]
-        public static async Task Run([ServiceBusTrigger("correct-implementation-netcore", Connection = "ServiceBusConnection")]
+        public static async Task Run(
+            [ServiceBusTrigger("correct-implementation-netstandard", Connection = "ServiceBusConnection")]
             Guid identifier,
             ILogger log)
         {
@@ -19,6 +21,20 @@ namespace CustomBindingFunction
 
             var baseAddress = Environment.GetEnvironmentVariable("BaseAddress", EnvironmentVariableTarget.Process);
             await HttpClient.GetAsync($"{baseAddress}api/Echo/{identifier}");
+
+            log.LogInformation($"Executed ServiceBus queue trigger. Processing message: {identifier}");
+        }
+
+        [FunctionName("CorrectImplementationWithBinding")]
+        public static void CorrectImplementationWithBinding(
+            [ServiceBusTrigger("correct-implementation-netstandard", Connection = "ServiceBusConnection")]
+            Guid identifier,
+            [Http(Url = "%BaseAddress%api/Echo/{identifier}")] HttpModel httpModel,
+            ILogger log)
+        {
+            log.LogInformation($"Executing ServiceBus queue trigger. Processing message: {identifier}");
+
+            log.LogInformation($"Retrieved status: {httpModel.ResponseCode} Content: {httpModel.Response}");
 
             log.LogInformation($"Executed ServiceBus queue trigger. Processing message: {identifier}");
         }
