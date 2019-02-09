f099999e46fcb1287d2bfe657ccb5fdfb794f3cb
Jan de Vries <jan@jan-v.nl>
2019-02-09 15:40:07 +0100

    fixed namespace

--- HealthChecking/Echo.cs
+++ HealthChecking/Echo.cs
@@ -1,10 +1,10 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
-namespace CustomBindingFunction
+namespace HealthChecking
 {
     public static class Echo
     {
