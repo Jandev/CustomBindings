cc238af0ab96c5c917593ad52175b1a0a2ae992f
Jan de Vries <jan@jan-v.nl>
2019-02-09 15:40:48 +0100

    fixed the correct implementation with the new Model sent to the queue and Getting/Posting it to the echo service

--- CustomBindingFunction/CorrectImplementation.cs
+++ CustomBindingFunction/CorrectImplementation.cs
@@ -9,6 +9,12 @@ using Newtonsoft.Json;
 
 namespace CustomBindingFunction
 {
+    /// <summary>
+    /// Correct implementation of an Azure Function using an <see cref="HttpClient"/>.
+    /// When testing, do make sure you only have 1 active Azure Function. Even if you just
+    /// comment out the <see cref="FunctionNameAttribute"/>, the <see cref="ServiceBusTriggerAttribute"/>
+    /// is still activated.
+    /// </summary>
     public static class CorrectImplementation
     {
         private static readonly HttpClient HttpClient = new HttpClient();
@@ -16,32 +22,52 @@ namespace CustomBindingFunction
         [FunctionName("CorrectImplementation")]
         public static async Task Run(
             [ServiceBusTrigger("correct-implementation-netstandard", Connection = "ServiceBusConnection")]
-            Guid identifier,
+            IncomingNetStandardModel netStandardModel,
             ILogger log)
         {
-            log.LogInformation($"Executing ServiceBus queue trigger. Processing message: {identifier}");
+            log.LogInformation($"Executing ServiceBus queue trigger. Processing message: {netStandardModel.Identifier}");
 
             var baseAddress = Environment.GetEnvironmentVariable("BaseAddress", EnvironmentVariableTarget.Process);
-            await HttpClient.GetAsync($"{baseAddress}api/Echo/{identifier}");
+            await HttpClient.GetAsync($"{baseAddress}api/Echo/{netStandardModel.Identifier}");
 
-            log.LogInformation($"Executed ServiceBus queue trigger. Processing message: {identifier}");
+            log.LogInformation($"Executed ServiceBus queue trigger. Processing message: {netStandardModel.Identifier}");
         }
 
         [FunctionName("CorrectImplementationWithBinding")]
         public static void CorrectImplementationWithBinding(
             [ServiceBusTrigger("correct-implementation-netstandard", Connection = "ServiceBusConnection")]
-            Guid identifier,
-            [Http(Url = "%BaseAddress%api/Echo/{identifier}")] 
+            IncomingNetStandardModel netStandardModel,
+            [Http(Url = "%BaseAddress%api/Echo/{Identifier}")] 
             HttpModel httpModel,
-            [HttpCommand(CommandUrl = "%BaseAddress%api/EchoCommand", MediaType = "application/json", HttpMethod = "POST")] 
+            ILogger log)
+        {
+            log.LogInformation($"Executing ServiceBus queue trigger. Processing message: {netStandardModel.Identifier}");
+
+            log.LogInformation($"Executed ServiceBus queue trigger. Processing message: {netStandardModel.Identifier}");
+        }
+
+        [FunctionName("CorrectImplementationWithPostBinding")]
+        public static void CorrectImplementationWithPostBinding(
+            [ServiceBusTrigger("correct-implementation-netstandard", Connection = "ServiceBusConnection")]
+            Guid identifier,
+            [HttpCommand(CommandUrl = "%BaseAddress%api/EchoCommand", MediaType = "application/json", HttpMethod = "POST")]
             IAsyncCollector<HttpCommand> httpPostCommandCollector,
             ILogger log)
         {
             log.LogInformation($"Executing ServiceBus queue trigger. Processing message: {identifier}");
 
-            httpPostCommandCollector.AddAsync(new HttpCommand(JsonConvert.SerializeObject(identifier)));
+            var model = new ModelToSend
+            {
+                Identifier = identifier.ToString("D")
+            };
+            httpPostCommandCollector.AddAsync(new HttpCommand(JsonConvert.SerializeObject(model)));
 
             log.LogInformation($"Executed ServiceBus queue trigger. Processing message: {identifier}");
+        }
+
+        private class ModelToSend
+        {
+            public string Identifier { get; set; }
         }
     }
 }
